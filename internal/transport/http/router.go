00e6a3041d58cdbb9dc2cf119a4bdd111216bcd7
W1nSec <winterisdeath@yandex.ru>
2023-11-27 23:18:31 +0300

    add http server

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -1,4 +1,4 @@
-package http
+package httpserver
 
 import (
 	"github.com/go-chi/chi/v5"
--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -1,1 +1,23 @@
-package http
+package httpserver
+
+import (
+	"net"
+	"net/http"
+)
+
+type HTTPServer struct {
+	addr   *net.TCPAddr
+	router *http.ServeMux
+}
+
+func NewHTTPServer(address string, router *http.ServeMux) (srv *HTTPServer, err error) {
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	return &HTTPServer{addr: addr, router: router}, nil
+}
+
+func (srv *HTTPServer) Run() error {
+	return http.ListenAndServe(srv.addr.String(), srv.router)
+}
